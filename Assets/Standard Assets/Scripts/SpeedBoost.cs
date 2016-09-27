cade08f817b88c355dd64cbe1f7cdd35b48e6fcc
Kevin-Nilsson <scnoobers@gmail.com>
2016-09-27 14:27:51 +0200

    ALL ZE GRAPHICS added audio to speedboost

---
+++ Assets/Standard Assets/Scripts/SpeedBoost.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoost : MonoBehaviour {
+
+    public float force = 50;
+
+    private AudioSource parentAudioSource;
+
+    void Start()
+    {
+        parentAudioSource = transform.parent.GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        var transformRight = transform.right;
+        var otherRbody = other.GetComponent<Rigidbody>();
+        
+        if (otherRbody && otherRbody.CompareTag("Player"))
+        {
+            otherRbody.AddForce(transformRight * force, ForceMode.Impulse);
+            parentAudioSource.Play();
+        }
+    }
+}
